6390233a32eb82394a16f5e3ab5aea2b72e3f91c
Prem Chandraboth <premchandraboth@ibots-beast.local>
2014-12-08 16:54:14 +0700

    Initial Commit

---
+++ location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  location
+//
+//  Created by Prem Chandraboth on 12/8/14.
+//  Copyright (c) 2014 Prem Chandraboth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
